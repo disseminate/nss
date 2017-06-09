95ed87c17852c3e6b7fde44a6e4fb3c68b25b8cb
Kyle Windsor <kwindsor@uwo.ca>
2017-06-09 00:36:12 -0400

    :art: sorted

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,10 +3,10 @@ include( "shared.lua" );
 include( "sv_player.lua" );
 include( "sv_subsystems.lua" );
 
-AddCSLuaFile( "shared.lua" );
-AddCSLuaFile( "sh_enum.lua" );
 AddCSLuaFile( "cl_gui.lua" );
-AddCSLuaFile( "cl_init.lua" );
 AddCSLuaFile( "cl_hud.lua" );
+AddCSLuaFile( "cl_init.lua" );
 AddCSLuaFile( "cl_skin.lua" );
 AddCSLuaFile( "cl_subsystems.lua" );
+AddCSLuaFile( "sh_enum.lua" );
+AddCSLuaFile( "shared.lua" );
